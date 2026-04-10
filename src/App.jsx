aaebf8bae3d98c5daf199e8918b263ab21393e4b
Asif Akbar <786asifakbar53@gmail.com>
2026-04-10 15:10:41 +0500

    fix: add missing line break in App component for improved readability

--- src/App.jsx
+++ src/App.jsx
@@ -17,6 +17,7 @@ function App() {
     <>
       {!ownerPath && <Navbar />}     
       <Routes>
+      
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
           <Route path="/hotels" element={<Hotels />} />
